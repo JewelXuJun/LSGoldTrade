b34841da311242ff6d8207907a80a3ae93515693
XuJun <xujun@rfinex.com>
2018-12-04 11:31:39 +0800

    资金划转流水tab UI

---
+++ app/src/main/java/com/jme/lsgoldtrade/ui/trade/TurnOverAdapter.java
@@ -0,0 +1,20 @@
+package com.jme.lsgoldtrade.ui.trade;
+
+import android.support.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+import java.util.List;
+
+public class TurnOverAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+
+    public TurnOverAdapter(int layoutResId, @Nullable List<String> data) {
+        super(layoutResId, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, String item) {
+
+    }
+}
--- app/src/main/java/com/jme/lsgoldtrade/ui/trade/TurnOverFragment.java
+++ app/src/main/java/com/jme/lsgoldtrade/ui/trade/TurnOverFragment.java
@@ -1,11 +1,70 @@
 package com.jme.lsgoldtrade.ui.trade;
 
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+
+import com.jme.common.network.DTRequest;
+import com.jme.common.network.Head;
+import com.jme.common.ui.view.MarginDividerItemDecoration;
 import com.jme.lsgoldtrade.R;
 import com.jme.lsgoldtrade.base.JMEBaseFragment;
+import com.jme.lsgoldtrade.databinding.FragmentTurnoverBinding;
 
 public class TurnOverFragment extends JMEBaseFragment {
+
+    private FragmentTurnoverBinding mBinding;
+
+    private TurnOverAdapter mAdapter;
+
+    private static final int TIME_START = 0;
+    private static final int TIME_END = 1;
+
     @Override
     protected int getContentViewId() {
         return R.layout.fragment_turnover;
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+
+        mBinding = (FragmentTurnoverBinding) mBindingUtil;
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+        super.initData(savedInstanceState);
+
+        mAdapter = new TurnOverAdapter(R.layout.item_turnover, null);
+
+        mBinding.recyclerView.addItemDecoration(new MarginDividerItemDecoration(mContext, LinearLayoutManager.VERTICAL));
+        mBinding.recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        mBinding.recyclerView.setAdapter(mAdapter);
+        mBinding.recyclerView.setNestedScrollingEnabled(false);
+    }
+
+    @Override
+    protected void initListener() {
+        super.initListener();
+    }
+
+    @Override
+    public void initBinding() {
+        super.initBinding();
+
+        mBinding.setHandlers(new ClickHandlers());
+    }
+
+    @Override
+    protected void DataReturn(DTRequest request, Head head, Object response) {
+        super.DataReturn(request, head, response);
+    }
+
+    public class ClickHandlers {
+
+        public void onClickTime(int timeType) {
+
+        }
+
     }
 }
