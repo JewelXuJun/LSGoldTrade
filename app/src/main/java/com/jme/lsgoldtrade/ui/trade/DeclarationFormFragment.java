4038185672d7b5c24e878df0e6e1a6b83dd98e17
XuJun <xujun@rfinex.com>
2018-12-04 16:32:23 +0800

    报单页面UI

--- app/src/main/java/com/jme/lsgoldtrade/ui/trade/DeclarationFormFragment.java
+++ app/src/main/java/com/jme/lsgoldtrade/ui/trade/DeclarationFormFragment.java
@@ -1,6 +1,12 @@
 package com.jme.lsgoldtrade.ui.trade;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.widget.LinearLayoutManager;
 
 import com.jme.common.network.DTRequest;
 import com.jme.common.network.Head;
@@ -11,6 +17,13 @@ import com.jme.lsgoldtrade.databinding.FragmentDeclarationFormBinding;
 public class DeclarationFormFragment extends JMEBaseFragment {
 
     private FragmentDeclarationFormBinding mBinding;
+
+    private Fragment[] mFragmentArrays;
+    private String[] mTabTitles;
+
+    private FiveSpeedAdapter mSaleAdapter;
+    private FiveSpeedAdapter mBuyAdapter;
+    private PagerAdapter mAdapter;
 
     @Override
     protected int getContentViewId() {
@@ -27,6 +40,17 @@ public class DeclarationFormFragment extends JMEBaseFragment {
     @Override
     protected void initData(Bundle savedInstanceState) {
         super.initData(savedInstanceState);
+
+        mAdapter = new TabViewPagerAdapter(getChildFragmentManager());
+        mSaleAdapter = new FiveSpeedAdapter(R.layout.item_five_speed, null);
+        mBuyAdapter = new FiveSpeedAdapter(R.layout.item_five_speed, null);
+
+        mBinding.recyclerViewSale.setLayoutManager(new LinearLayoutManager(mContext));
+        mBinding.recyclerViewSale.setAdapter(mSaleAdapter);
+        mBinding.recyclerViewBuy.setLayoutManager(new LinearLayoutManager(mContext));
+        mBinding.recyclerViewBuy.setAdapter(mBuyAdapter);
+
+        initInfoTabs();
     }
 
     @Override
@@ -38,7 +62,33 @@ public class DeclarationFormFragment extends JMEBaseFragment {
     public void initBinding() {
         super.initBinding();
 
+        mBinding.setHandlers(new ClickHandlers());
+    }
 
+    private void initInfoTabs() {
+        mTabTitles = new String[4];
+        mTabTitles[0] = mContext.getResources().getString(R.string.trade_hold_position);
+        mTabTitles[1] = mContext.getResources().getString(R.string.trade_entrust);
+        mTabTitles[2] = mContext.getResources().getString(R.string.trade_deal);
+        mTabTitles[3] = mContext.getResources().getString(R.string.trade_cancel_order);
+
+        mFragmentArrays = new Fragment[4];
+        mFragmentArrays[0] = new ItemHoldPositionFragment();
+        mFragmentArrays[1] = new ItemEntrustFragment();
+        mFragmentArrays[2] = new ItemDealFragment();
+        mFragmentArrays[3] = new ItemCancelOrderFragment();
+
+        initTabLayout();
+    }
+
+    private void initTabLayout() {
+        mBinding.tabViewpager.removeAllViewsInLayout();
+        mBinding.tabViewpager.setAdapter(mAdapter);
+        mBinding.tabViewpager.setOffscreenPageLimit(4);
+        mBinding.tablayout.setTabMode(TabLayout.MODE_FIXED);
+        mBinding.tablayout.setSelectedTabIndicatorHeight(4);
+        mBinding.tablayout.setupWithViewPager(mBinding.tabViewpager);
+        mBinding.tablayout.post(() -> setIndicator(mBinding.tablayout, 30, 30));
     }
 
     @Override
@@ -48,5 +98,55 @@ public class DeclarationFormFragment extends JMEBaseFragment {
 
     public class ClickHandlers {
 
+        public void onClickPriceMinus() {
+
+        }
+
+        public void onClickPriceAdd() {
+
+        }
+
+        public void onClickAmountMinus() {
+
+        }
+
+        public void onClickAmountAdd() {
+
+        }
+
+        public void onClickBuyMore() {
+
+        }
+
+        public void onClickSaleEmpty() {
+
+        }
+
+        public void onClickEveningUp() {
+
+        }
+
     }
+
+    final class TabViewPagerAdapter extends FragmentPagerAdapter {
+        public TabViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentArrays[position];
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentArrays.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTabTitles[position];
+        }
+    }
+
 }
---
+++ app/src/main/java/com/jme/lsgoldtrade/ui/trade/ItemHoldPositionFragment.java
@@ -0,0 +1,11 @@
+package com.jme.lsgoldtrade.ui.trade;
+
+import com.jme.lsgoldtrade.R;
+import com.jme.lsgoldtrade.base.JMEBaseFragment;
+
+public class ItemHoldPositionFragment extends JMEBaseFragment {
+    @Override
+    protected int getContentViewId() {
+        return R.layout.fragment_item_hold_position;
+    }
+}
