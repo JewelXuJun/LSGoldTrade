c4265a613804c62fff4688ee276a62fb59c028a2
XuJun <xujun@rfinex.com>
2018-12-04 18:21:09 +0800

    报单页面tab UI

--- app/src/main/java/com/jme/lsgoldtrade/ui/trade/ItemHoldPositionFragment.java
+++ app/src/main/java/com/jme/lsgoldtrade/ui/trade/ItemHoldPositionFragment.java
@@ -1,11 +1,58 @@
 package com.jme.lsgoldtrade.ui.trade;
 
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+
+import com.jme.common.network.DTRequest;
+import com.jme.common.network.Head;
+import com.jme.common.ui.view.MarginDividerItemDecoration;
 import com.jme.lsgoldtrade.R;
 import com.jme.lsgoldtrade.base.JMEBaseFragment;
+import com.jme.lsgoldtrade.databinding.FragmentItemHoldPositionBinding;
 
 public class ItemHoldPositionFragment extends JMEBaseFragment {
+
+    private FragmentItemHoldPositionBinding mBinding;
+
+    private HoldPositionAdapter mAdapter;
+
     @Override
     protected int getContentViewId() {
         return R.layout.fragment_item_hold_position;
     }
+
+    @Override
+    protected void initView() {
+        super.initView();
+
+        mBinding = (FragmentItemHoldPositionBinding) mBindingUtil;
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+        super.initData(savedInstanceState);
+
+        mAdapter = new HoldPositionAdapter(R.layout.item_order_hold_position, null);
+
+        mBinding.recyclerView.addItemDecoration(new MarginDividerItemDecoration(mContext, LinearLayoutManager.VERTICAL));
+        mBinding.recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        mBinding.recyclerView.setAdapter(mAdapter);
+        mBinding.recyclerView.setNestedScrollingEnabled(false);
+    }
+
+    @Override
+    protected void initListener() {
+        super.initListener();
+    }
+
+    @Override
+    public void initBinding() {
+        super.initBinding();
+    }
+
+    @Override
+    protected void DataReturn(DTRequest request, Head head, Object response) {
+        super.DataReturn(request, head, response);
+    }
+
 }
