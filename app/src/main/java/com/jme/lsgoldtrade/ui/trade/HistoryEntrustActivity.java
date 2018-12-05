2d49f7b5f700f4817757c414645e751e7846f3f8
XuJun <xujun@rfinex.com>
2018-12-05 15:55:34 +0800

    五档修改为十档以及当日成交和历史成交UI

--- app/src/main/java/com/jme/lsgoldtrade/ui/trade/HistoryEntrustActivity.java
+++ app/src/main/java/com/jme/lsgoldtrade/ui/trade/HistoryEntrustActivity.java
@@ -1,12 +1,10 @@
 package com.jme.lsgoldtrade.ui.trade;
 
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.jme.common.network.DTRequest;
 import com.jme.common.network.Head;
-import com.jme.common.ui.view.MarginDividerItemDecoration;
 import com.jme.lsgoldtrade.R;
 import com.jme.lsgoldtrade.base.JMEBaseActivity;
 import com.jme.lsgoldtrade.config.Constants;
@@ -38,10 +36,6 @@ public class HistoryEntrustActivity extends JMEBaseActivity {
         super.initData(savedInstanceState);
 
         mAdapter = new EntrustAdapter(R.layout.item_entrust, null);
-
-        mBinding.recyclerView.addItemDecoration(new MarginDividerItemDecoration(mContext, LinearLayoutManager.VERTICAL));
-        mBinding.recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
-        mBinding.recyclerView.setAdapter(mAdapter);
     }
 
     @Override
