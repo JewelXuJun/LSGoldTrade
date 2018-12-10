8b6e1b57d1e5c31c46980e14cafab4656e7c4245
XuJun <xujun@rfinex.com>
2018-12-10 22:34:54 +0800

    行情刷新页面功能实现

--- app/src/main/java/com/jme/lsgoldtrade/ui/personal/MarketRefreshActivity.java
+++ app/src/main/java/com/jme/lsgoldtrade/ui/personal/MarketRefreshActivity.java
@@ -1,10 +1,13 @@
 package com.jme.lsgoldtrade.ui.personal;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.jme.common.util.SharedPreUtils;
 import com.jme.lsgoldtrade.R;
 import com.jme.lsgoldtrade.base.JMEBaseActivity;
+import com.jme.lsgoldtrade.config.AppConfig;
 import com.jme.lsgoldtrade.config.Constants;
 import com.jme.lsgoldtrade.databinding.ActivityMarketRefreshBinding;
 
@@ -30,6 +33,9 @@ public class MarketRefreshActivity extends JMEBaseActivity {
     @Override
     protected void initData(Bundle savedInstanceState) {
         super.initData(savedInstanceState);
+
+        setNetWorkSeconds(AppConfig.TimeInterval_NetWork);
+        setWiFiSeconds(AppConfig.TimeInterval_WiFi);
     }
 
     @Override
@@ -44,14 +50,38 @@ public class MarketRefreshActivity extends JMEBaseActivity {
         mBinding.setHandlers(new ClickHandlers());
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreUtils.setLong(this, SharedPreUtils.TimeInterval_NetWork, AppConfig.TimeInterval_NetWork);
+        SharedPreUtils.setLong(this, SharedPreUtils.TimeInterval_WiFi, AppConfig.TimeInterval_WiFi);
+    }
+
+    private void setNetWorkSeconds(long seconds) {
+        mBinding.imgNetwork5s.setVisibility(seconds == AppConfig.Second5 ? View.VISIBLE : View.GONE);
+        mBinding.imgNetwork10s.setVisibility(seconds == AppConfig.Second10 ? View.VISIBLE : View.GONE);
+        mBinding.imgNetwork15s.setVisibility(seconds == AppConfig.Second15 ? View.VISIBLE : View.GONE);
+
+        AppConfig.TimeInterval_NetWork = seconds;
+    }
+
+    private void setWiFiSeconds(long seconds) {
+        mBinding.imgWifi1s.setVisibility(seconds == AppConfig.Second ? View.VISIBLE : View.GONE);
+        mBinding.imgWifi2s.setVisibility(seconds == AppConfig.Second2 ? View.VISIBLE : View.GONE);
+        mBinding.imgWifi5s.setVisibility(seconds == AppConfig.Second5 ? View.VISIBLE : View.GONE);
+
+        AppConfig.TimeInterval_WiFi = seconds;
+    }
+
     public class ClickHandlers {
 
-        public void ClickNetWorkSeconds(int seconds) {
-
+        public void ClickNetWorkSeconds(long seconds) {
+            setNetWorkSeconds(seconds);
         }
 
-        public void ClickWiFiSeconds(int seconds) {
-
+        public void ClickWiFiSeconds(long seconds) {
+            setWiFiSeconds(seconds);
         }
 
     }
