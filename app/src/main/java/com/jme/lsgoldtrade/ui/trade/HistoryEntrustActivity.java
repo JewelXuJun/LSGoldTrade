5f34ee82777fb0125f0acedda5be8ec6d4bf36fe
XuJun <xujun@rfinex.com>
2019-01-02 23:22:33 +0800

    历史委托时间选择

--- app/src/main/java/com/jme/lsgoldtrade/ui/trade/HistoryEntrustActivity.java
+++ app/src/main/java/com/jme/lsgoldtrade/ui/trade/HistoryEntrustActivity.java
@@ -1,21 +1,43 @@
 package com.jme.lsgoldtrade.ui.trade;
 
+import android.annotation.TargetApi;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.jme.common.network.DTRequest;
 import com.jme.common.network.Head;
+import com.jme.common.ui.view.MarginDividerItemDecoration;
+import com.jme.common.util.DateUtil;
 import com.jme.lsgoldtrade.R;
 import com.jme.lsgoldtrade.base.JMEBaseActivity;
 import com.jme.lsgoldtrade.config.Constants;
 import com.jme.lsgoldtrade.databinding.ActivityHistoryEntrustBinding;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
+
+import java.util.Calendar;
+import java.util.Date;
 
 @Route(path = Constants.ARouterUriConst.HISTORYENTRUST)
-public class HistoryEntrustActivity extends JMEBaseActivity {
+public class HistoryEntrustActivity extends JMEBaseActivity implements OnRefreshListener, BaseQuickAdapter.RequestLoadMoreListener {
 
     private ActivityHistoryEntrustBinding mBinding;
 
     private EntrustAdapter mAdapter;
+    private DatePickerDialog mDatePickerDialog;
+
+    private long mStartTime = 0;
+    private long mEndTime = 0;
+    private int mYear;
+    private int mMonth;
+    private int mDayOfMonth;
+
+    private static final int TIME_START = 0;
+    private static final int TIME_END = 1;
 
     @Override
     protected int getContentViewId() {
@@ -36,11 +58,22 @@ public class HistoryEntrustActivity extends JMEBaseActivity {
         super.initData(savedInstanceState);
 
         mAdapter = new EntrustAdapter(R.layout.item_entrust, null, "History");
+        mAdapter.clearDate();
+
+        mBinding.recyclerView.setHasFixedSize(false);
+        mBinding.recyclerView.addItemDecoration(new MarginDividerItemDecoration(mContext, LinearLayoutManager.VERTICAL));
+        mBinding.recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        mBinding.recyclerView.setAdapter(mAdapter);
+
+        initDate();
     }
 
     @Override
     protected void initListener() {
         super.initListener();
+
+        mAdapter.setOnLoadMoreListener(this, mBinding.recyclerView);
+        mBinding.swipeRefreshLayout.setOnRefreshListener(this);
     }
 
     @Override
@@ -50,15 +83,108 @@ public class HistoryEntrustActivity extends JMEBaseActivity {
         mBinding.setHandlers(new ClickHandlers());
     }
 
+    private void initDate() {
+        getTodayCalendar();
+
+        mStartTime = getStartTime(mYear, mMonth, mDayOfMonth);
+        mEndTime = getEndTime(mYear, mMonth, mDayOfMonth);
+
+        mBinding.tvStartTime.setText(DateUtil.dateToString(mStartTime));
+        mBinding.tvEndTime.setText(DateUtil.dateToString(mEndTime));
+    }
+
+    private void getTodayCalendar() {
+        Calendar calendar = Calendar.getInstance();
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    private long getStartTime(int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        return calendar.getTimeInMillis();
+    }
+
+    private long getEndTime(int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        return calendar.getTimeInMillis();
+    }
+
+    private void setStartTime(int year, int month, int dayOfMonth) {
+        long startTime = getStartTime(year, month, dayOfMonth);
+
+        if (startTime <= mEndTime) {
+            mStartTime = startTime;
+
+            mBinding.tvStartTime.setText(DateUtil.dateToString(mStartTime));
+        } else {
+            showShortToast(R.string.trade_start_time_error);
+        }
+    }
+
+    private void setEndTime(int year, int month, int dayOfMonth) {
+        long endTime = getEndTime(year, month, dayOfMonth);
+
+        if (endTime >= mStartTime) {
+            mEndTime = endTime;
+
+            mBinding.tvEndTime.setText(DateUtil.dateToString(mEndTime));
+        } else {
+            showShortToast(R.string.trade_end_time_error);
+        }
+    }
+
     @Override
     protected void DataReturn(DTRequest request, Head head, Object response) {
         super.DataReturn(request, head, response);
     }
 
+    @Override
+    public void onLoadMoreRequested() {
+
+    }
+
+    @Override
+    public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+
+    }
+
     public class ClickHandlers {
 
-        public void onClickSelectDate(int position) {
+        @TargetApi(24)
+        public void onClickSelectDate(int timeType) {
+            if (null != mDatePickerDialog && mDatePickerDialog.isShowing())
+                return;
 
+            getTodayCalendar();
+
+            mDatePickerDialog = new DatePickerDialog(HistoryEntrustActivity.this, (datePicker, year, month, dayOfMonth) -> {
+                if (timeType == TIME_START)
+                    setStartTime(year, month, dayOfMonth);
+                else if (timeType == TIME_END)
+                    setEndTime(year, month, dayOfMonth);
+            }, mYear, mMonth, mDayOfMonth);
+
+            if (android.os.Build.VERSION.SDK_INT >= 11)
+                mDatePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
+
+            mDatePickerDialog.show();
         }
 
     }
