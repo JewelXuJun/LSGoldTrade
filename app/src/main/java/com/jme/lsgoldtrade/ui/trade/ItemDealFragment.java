1d7d927c852bcfc007dc8b435fefabe63798a9a5
XuJun <xujun@rfinex.com>
2019-01-03 22:12:44 +0800

    报单成交页面逻辑

--- app/src/main/java/com/jme/lsgoldtrade/ui/trade/ItemDealFragment.java
+++ app/src/main/java/com/jme/lsgoldtrade/ui/trade/ItemDealFragment.java
@@ -3,18 +3,29 @@ package com.jme.lsgoldtrade.ui.trade;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.jme.common.network.DTRequest;
 import com.jme.common.network.Head;
 import com.jme.common.ui.view.MarginDividerItemDecoration;
 import com.jme.lsgoldtrade.R;
 import com.jme.lsgoldtrade.base.JMEBaseFragment;
 import com.jme.lsgoldtrade.databinding.FragmentItemDealBinding;
+import com.jme.lsgoldtrade.domain.DealPageVo;
+import com.jme.lsgoldtrade.service.TradeService;
 
-public class ItemDealFragment extends JMEBaseFragment {
+import java.util.HashMap;
+import java.util.List;
+
+public class ItemDealFragment extends JMEBaseFragment implements BaseQuickAdapter.RequestLoadMoreListener{
 
     private FragmentItemDealBinding mBinding;
 
-    private ItemDealAdapter mAdapter;
+    private DealAdapter mAdapter;
+
+    private boolean bVisibleToUser = false;
+
+    private int mCurrentPage = 1;
+    private boolean bHasNext = false;
 
     @Override
     protected int getContentViewId() {
@@ -32,8 +43,9 @@ public class ItemDealFragment extends JMEBaseFragment {
     protected void initData(Bundle savedInstanceState) {
         super.initData(savedInstanceState);
 
-        mAdapter = new ItemDealAdapter(R.layout.item_order_deal, null);
+        mAdapter = new DealAdapter(R.layout.item_deal, null, "Current");
 
+        mBinding.recyclerView.setHasFixedSize(false);
         mBinding.recyclerView.addItemDecoration(new MarginDividerItemDecoration(mContext, LinearLayoutManager.VERTICAL));
         mBinding.recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
         mBinding.recyclerView.setAdapter(mAdapter);
@@ -43,6 +55,8 @@ public class ItemDealFragment extends JMEBaseFragment {
     @Override
     protected void initListener() {
         super.initListener();
+
+        mAdapter.setOnLoadMoreListener(this, mBinding.recyclerView);
     }
 
     @Override
@@ -51,7 +65,92 @@ public class ItemDealFragment extends JMEBaseFragment {
     }
 
     @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        bVisibleToUser = isVisibleToUser;
+
+        super.setUserVisibleHint(isVisibleToUser);
+
+      /*  if (null != mBinding && bVisibleToUser)
+            initDealPage();*/
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        bVisibleToUser = !hidden;
+
+        super.onHiddenChanged(hidden);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        /*if (bVisibleToUser)
+            initDealPage();*/
+    }
+
+    private void initDealPage() {
+        mCurrentPage = 1;
+
+        dealpage();
+    }
+
+    private void dealpage() {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("pageNo", String.valueOf(mCurrentPage));
+
+        sendRequest(TradeService.getInstance().dealpage, params, false, false, false);
+    }
+
+    @Override
     protected void DataReturn(DTRequest request, Head head, Object response) {
         super.DataReturn(request, head, response);
+
+        switch (request.getApi().getName()) {
+            case "OrderPage":
+                if (head.isSuccess()) {
+                    DealPageVo dealPageVo;
+
+                    try {
+                        dealPageVo = (DealPageVo) response;
+                    } catch (Exception e) {
+                        dealPageVo = null;
+
+                        e.printStackTrace();
+                    }
+
+                    if (null == dealPageVo)
+                        return;
+
+                    bHasNext = dealPageVo.isHasNext();
+
+                    List<DealPageVo.DealBean> dealBeanList = dealPageVo.getList();
+
+                    if (mCurrentPage == 1) {
+                        mAdapter.setNewData(dealBeanList);
+
+                        if (null != dealBeanList && 0 < dealBeanList.size())
+                            mAdapter.loadMoreComplete();
+                    } else {
+                        mAdapter.addData(dealBeanList);
+                        mAdapter.loadMoreComplete();
+                    }
+                }
+
+                break;
+        }
+    }
+
+    @Override
+    public void onLoadMoreRequested() {
+        mBinding.recyclerView.postDelayed(() -> {
+            if (bHasNext) {
+                mCurrentPage++;
+
+                dealpage();
+            } else {
+                mAdapter.loadMoreEnd();
+            }
+        }, 0);
     }
 }
