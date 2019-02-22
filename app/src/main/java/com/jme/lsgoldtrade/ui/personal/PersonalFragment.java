7ff17bfa6ec1a619519b06685055180abb6863b9
XuJun <xujun@rfinex.com>
2019-02-22 14:28:08 +0800

    分享功能提示敬请期待

--- app/src/main/java/com/jme/lsgoldtrade/ui/personal/PersonalFragment.java
+++ app/src/main/java/com/jme/lsgoldtrade/ui/personal/PersonalFragment.java
@@ -97,7 +97,7 @@ public class PersonalFragment extends JMEBaseFragment {
         }
 
         public void onClickShare() {
-
+            showShortToast(R.string.setting_expect);
         }
 
         public void onClickFeedback() {
