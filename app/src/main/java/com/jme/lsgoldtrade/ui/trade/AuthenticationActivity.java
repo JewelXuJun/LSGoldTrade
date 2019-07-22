7111643406ca2436ed1b1b0ef745bc700faa188c
XuJun <xujun@rfinex.com>
2019-07-22 14:29:02 +0800

    交易页面免费开户功能实现

--- app/src/main/java/com/jme/lsgoldtrade/ui/trade/AuthenticationActivity.java
+++ app/src/main/java/com/jme/lsgoldtrade/ui/trade/AuthenticationActivity.java
@@ -7,6 +7,7 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.jme.common.network.DTRequest;
 import com.jme.common.network.Head;
+import com.jme.common.util.IDUtils;
 import com.jme.lsgoldtrade.R;
 import com.jme.lsgoldtrade.base.JMEBaseActivity;
 import com.jme.lsgoldtrade.config.Constants;
@@ -25,9 +26,8 @@ import java.util.HashMap;
 public class AuthenticationActivity extends JMEBaseActivity {
 
     private ActivityAuthenticationBinding mBinding;
-    private String tag;
-    private String name;
-    private String idCard;
+
+    private String mType;
 
     @Override
     protected int getContentViewId() {
@@ -37,82 +37,86 @@ public class AuthenticationActivity extends JMEBaseActivity {
     @Override
     protected void initView() {
         super.initView();
-        mBinding = (ActivityAuthenticationBinding) mBindingUtil;
-        initToolbar("身份验证", true);
+
+        initToolbar(R.string.trade_authentication, true);
     }
 
     @Override
     protected void initData(Bundle savedInstanceState) {
         super.initData(savedInstanceState);
-        tag = getIntent().getStringExtra("Type");
-        if ("1".equals(tag)) {
-            mBinding.btnBind.setText("立即绑定");
-        } else {
-            mBinding.btnBind.setText("下一步");
-        }
-        getDataFromNet();
-    }
 
-    private void getDataFromNet() {
-        sendRequest(TradeService.getInstance().whetherIdCard, new HashMap<>(), true);
+        mType = getIntent().getStringExtra("Type");
+
+        mBinding.btnBind.setText(mType.equals("1") ? R.string.register_open_account : R.string.text_next);
+
+        getWhetherIdCard();
     }
 
     @Override
     protected void initListener() {
         super.initListener();
-
     }
 
     @Override
     protected void initBinding() {
         super.initBinding();
+
+        mBinding = (ActivityAuthenticationBinding) mBindingUtil;
         mBinding.setHandlers(new ClickHandlers());
+    }
+
+    private void getWhetherIdCard() {
+        sendRequest(TradeService.getInstance().whetherIdCard, new HashMap<>(), true);
+    }
+
+    private void verifyIdCard(String name, String idCard) {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("idCard", idCard);
+
+        sendRequest(TradeService.getInstance().verifyIdCard, params, true);
     }
 
     @Override
     protected void DataReturn(DTRequest request, Head head, Object response) {
         super.DataReturn(request, head, response);
+
         switch (request.getApi().getName()) {
             case "WhetherIdCard":
                 if (head.isSuccess()) {
-
-                    IdentityInfoVo value;
+                    IdentityInfoVo identityInfoVo;
 
                     try {
-                        value = (IdentityInfoVo) response;
+                        identityInfoVo = (IdentityInfoVo) response;
                     } catch (Exception e) {
-                        value = null;
+                        identityInfoVo = null;
+
                         e.printStackTrace();
                     }
 
-                    if (value == null) {
+                    if (null == identityInfoVo)
                         return;
-                    }
 
-                    name = value.getName();
-                    idCard = value.getIdCard();
-
-                    mBinding.etName.setText(name);
-                    mBinding.etNameCard.setText(idCard);
+                    mBinding.etName.setText(identityInfoVo.getName());
+                    mBinding.etIdCard.setText(identityInfoVo.getIdCard());
                 }
+
                 break;
             case "VerifyIdCard":
-                mBinding.btnBind.setClickable(true);
                 if (head.isSuccess()) {
-                    if ("1".equals(tag)) {
-                        IntentUtils.jumpBankSmall(mContext);
+                    if (mType.equals("1")) {
+                        IntentUtils.jumpBankSmall(this);
                     } else {
                         String name = mBinding.etName.getText().toString().trim();
-                        String namecard = mBinding.etNameCard.getText().toString().trim();
+                        String namecard = mBinding.etIdCard.getText().toString().trim();
                         ARouter.getInstance()
                                 .build(Constants.ARouterUriConst.BINDACCOUNT)
                                 .withString("name", name)
                                 .withString("card", namecard)
                                 .navigation();
                     }
-                } else {
-                    showShortToast(head.getMsg());
                 }
+
                 break;
         }
     }
@@ -121,24 +125,17 @@ public class AuthenticationActivity extends JMEBaseActivity {
 
         public void onClickBind() {
             String name = mBinding.etName.getText().toString().trim();
-            String nameCard = mBinding.etNameCard.getText().toString().trim();
-            if (TextUtils.isEmpty(name)) {
-                showShortToast("请输入您的名字");
-                return;
-            }
-            if (TextUtils.isEmpty(nameCard)) {
-                showShortToast("请输入您的身份证号");
-                return;
-            }
-            if (!NormalUtils.isIdCardNum(nameCard)) {
-                showShortToast("请输入有效身份证号");
-                return;
-            }
-            HashMap<String, String> params = new HashMap<>();
-            params.put("name", name);
-            params.put("idCard", nameCard);
-            sendRequest(TradeService.getInstance().verifyIdCard, params, false);
-            mBinding.btnBind.setClickable(false);
+            String idCard = mBinding.etIdCard.getText().toString().trim();
+            String errorMsg = IDUtils.IDCardValidate(idCard);
+
+            if (TextUtils.isEmpty(name))
+                showShortToast(R.string.trade_name_hint);
+            else if (TextUtils.isEmpty(idCard))
+                showShortToast(R.string.trade_id_card_hint);
+            else if (!TextUtils.isEmpty(errorMsg))
+                showShortToast(errorMsg);
+            else
+                verifyIdCard(name, idCard);
         }
     }
 }
