a103304f94ef16965c3f500ca6b3d9c9fe56dad7
XuJun <xujun@rfinex.com>
2019-08-31 23:42:49 +0800

    电子账户弹框

---
+++ app/src/main/java/com/jme/lsgoldtrade/view/ConfirmSimplePopupwindow.java
@@ -0,0 +1,47 @@
+package com.jme.lsgoldtrade.view;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jme.common.util.DensityUtil;
+import com.jme.lsgoldtrade.R;
+import com.jme.lsgoldtrade.base.JMEBasePopupWindow;
+import com.jme.lsgoldtrade.databinding.PopupwindowConfirmBinding;
+import com.jme.lsgoldtrade.databinding.PopupwindowConfirmSimpleBinding;
+
+public class ConfirmSimplePopupwindow extends JMEBasePopupWindow {
+
+    private PopupwindowConfirmSimpleBinding mBinding;
+
+    public ConfirmSimplePopupwindow(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void initPopupWindow() {
+        super.initPopupWindow();
+
+        setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        setWidth(DensityUtil.dpTopx(getContext(), 295));
+    }
+
+    @Override
+    public void iniWindow() {
+        super.iniWindow();
+
+        mBinding = DataBindingUtil.inflate(LayoutInflater.from(getContext()), R.layout.popupwindow_confirm_simple, null, false);
+
+        if (null == mBinding)
+            return;
+
+        setContentView(mBinding.getRoot());
+    }
+
+    public void setData(String content, View.OnClickListener confirmListener) {
+        mBinding.tvContent.setText(content);
+        mBinding.btnConfirm.setOnClickListener(confirmListener);
+    }
+}
