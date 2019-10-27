0edc17de19d381f8c9883c9c5db28f769c2dc6af
XuJun <xujun@rfinex.com>
2019-10-28 00:23:36 +0800

    手势密码启用页面

--- app/src/main/java/com/jme/lsgoldtrade/ui/security/GestureActivity.java
+++ app/src/main/java/com/jme/lsgoldtrade/ui/security/GestureActivity.java
@@ -1,6 +1,8 @@
 package com.jme.lsgoldtrade.ui.security;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.jme.common.network.DTRequest;
@@ -9,11 +11,17 @@ import com.jme.lsgoldtrade.R;
 import com.jme.lsgoldtrade.base.JMEBaseActivity;
 import com.jme.lsgoldtrade.config.Constants;
 import com.jme.lsgoldtrade.databinding.ActivityGestureBinding;
+import com.jme.lsgoldtrade.domain.PasswordInfoVo;
+import com.jme.lsgoldtrade.service.ManagementService;
+
+import java.util.HashMap;
 
 @Route(path = Constants.ARouterUriConst.GESTURE)
 public class GestureActivity extends JMEBaseActivity {
 
     private ActivityGestureBinding mBinding;
+
+    private boolean bFlag = true;
 
     @Override
     protected int getContentViewId() {
@@ -35,6 +43,11 @@ public class GestureActivity extends JMEBaseActivity {
     @Override
     protected void initListener() {
         super.initListener();
+
+        mBinding.switchView.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (bFlag)
+                return;
+        });
     }
 
     @Override
@@ -46,8 +59,49 @@ public class GestureActivity extends JMEBaseActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        bFlag = true;
+
+        getUserPasswordSettingInfo();
+    }
+
+    private void getUserPasswordSettingInfo() {
+        sendRequest(ManagementService.getInstance().getUserPasswordSettingInfo, new HashMap<>(), bFlag, false, bFlag);
+    }
+
+    @Override
     protected void DataReturn(DTRequest request, Head head, Object response) {
         super.DataReturn(request, head, response);
+
+        switch (request.getApi().getName()) {
+            case "GetUserPasswordSettingInfo":
+                if (head.isSuccess()) {
+                    PasswordInfoVo passwordInfoVo;
+
+                    try {
+                        passwordInfoVo = (PasswordInfoVo) response;
+                    } catch (Exception e) {
+                        passwordInfoVo = null;
+
+                        e.printStackTrace();
+                    }
+
+                    if (null == passwordInfoVo)
+                        return;
+
+                    String hasOpenGestures = passwordInfoVo.getHasOpenGestures();
+
+                    mBinding.switchView.setChecked(TextUtils.isEmpty(hasOpenGestures) ? false : hasOpenGestures.equals("Y"));
+                    mBinding.layoutGestureModify.setVisibility(TextUtils.isEmpty(hasOpenGestures) ? View.GONE
+                            : hasOpenGestures.equals("Y") ? View.VISIBLE : View.GONE);
+                }
+
+                bFlag = false;
+
+                break;
+        }
     }
 
     public class ClickHandlers {
