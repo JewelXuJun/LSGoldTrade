1f6b9a20a44c5318247b442a151e019929b482f7
XuJun <xujun@rfinex.com>
2019-11-02 11:34:36 +0800

    条件单tabhost

--- app/src/main/java/com/jme/lsgoldtrade/ui/transaction/ConditionSheetActivity.java
+++ app/src/main/java/com/jme/lsgoldtrade/ui/transaction/ConditionSheetActivity.java
@@ -1,10 +1,22 @@
 package com.jme.lsgoldtrade.ui.transaction;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.google.android.material.tabs.TabLayout;
+import com.jme.common.util.DensityUtil;
+import com.jme.common.util.ScreenUtil;
 import com.jme.lsgoldtrade.R;
 import com.jme.lsgoldtrade.base.JMEBaseActivity;
+import com.jme.lsgoldtrade.base.TabViewPagerAdapter;
 import com.jme.lsgoldtrade.config.Constants;
 import com.jme.lsgoldtrade.databinding.ActivityConditionSheetBinding;
 
@@ -12,6 +24,11 @@ import com.jme.lsgoldtrade.databinding.ActivityConditionSheetBinding;
 public class ConditionSheetActivity extends JMEBaseActivity {
 
     private ActivityConditionSheetBinding mBinding;
+
+    private String[] mTabTitles;
+    private Fragment[] mFragments;
+
+    private TabViewPagerAdapter mAdapter;
 
     @Override
     protected int getContentViewId() {
@@ -30,6 +47,8 @@ public class ConditionSheetActivity extends JMEBaseActivity {
     @Override
     protected void initData(Bundle savedInstanceState) {
         super.initData(savedInstanceState);
+
+        initTabLayout();
     }
 
     @Override
@@ -42,6 +61,45 @@ public class ConditionSheetActivity extends JMEBaseActivity {
         super.initBinding();
 
         mBinding = (ActivityConditionSheetBinding) mBindingUtil;
+    }
+
+    private void initTabLayout() {
+        mTabTitles = new String[3];
+        mTabTitles[0] = getResources().getString(R.string.transaction_create_condition_sheet);
+        mTabTitles[1] = getResources().getString(R.string.transaction_own_condition_sheet);
+        mTabTitles[2] = mContext.getResources().getString(R.string.transaction_stop_sheet);
+
+        mFragments = new Fragment[3];
+        mFragments[0] = new CreateConditionSheetFragment();
+        mFragments[1] = new OwnConditionSheetFragment();
+        mFragments[2] = new TransactionStopSheetFragment();
+
+        mAdapter = new TabViewPagerAdapter(getSupportFragmentManager(), mTabTitles, mFragments);
+
+        mBinding.tabViewpager.removeAllViewsInLayout();
+        mBinding.tabViewpager.setAdapter(mAdapter);
+        mBinding.tabViewpager.setOffscreenPageLimit(3);
+        mBinding.tablayout.setupWithViewPager(mBinding.tabViewpager);
+
+        for (int i = 0; i < mBinding.tablayout.getTabCount(); i++) {
+            TabLayout.Tab tab = mBinding.tablayout.getTabAt(i);
+
+            if (null != tab)
+                tab.setCustomView(getTabView(i));
+        }
+    }
+
+    private View getTabView(int currentPosition) {
+        View view = LayoutInflater.from(this).inflate(R.layout.item_tab, null);
+        TextView textView = view.findViewById(R.id.tv_tab);
+        textView.setText(mTabTitles[currentPosition]);
+        textView.setGravity(Gravity.CENTER);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        params.width = (ScreenUtil.getScreenWidth(this) - DensityUtil.dpTopx(this, 30)) / 3;
+        params.height = DensityUtil.dpTopx(this, 31);
+        textView.setLayoutParams(params);
+
+        return view;
     }
 
 }
